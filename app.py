e5eef117d7f2cee86d98a114dac13d1b3d0b79c3
Kirill Klem <klemkirill20@gmail.com>
2023-03-26 20:01:08 +0300

    Add schedule

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import requests
 import settings
 from yarl import URL
 
+import schedule
+import time
+
 fbref = sd.FBref(leagues=['ENG-Premier League'], seasons='22-23')
 
 stats = fbref.read_player_season_stats(stat_type='standard')
 url = URL(settings.BASE_API_URL)
-r = requests.post(str(url), json=stats.to_json())
 
+def request_post():
+    r = requests.post(str(url), json=stats.to_json())
+
+schedule.every().hour.do(request_post())
