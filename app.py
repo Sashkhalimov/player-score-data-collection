1819c50cf64789db985cac4e945d6cb894d72084
Kirill Klem <klemkirill20@gmail.com>
2023-03-29 19:27:45 +0300

    fixed 'orient' on 'index'

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import schedule
 import soccerdata as sd
 from yarl import URL
+from pprint import pprint
 
 from loguru import logger
 
@@ -16,11 +17,11 @@ url = URL(settings.BASE_API_URL) / 'api' / 'player'
 def update_players_data():
     logger.debug('chto ugodno')
     stats = fbref.read_player_season_stats(stat_type='standard')
-    r = requests.post(str(url), json=stats.to_json())
-
-    
+    r = requests.post(str(url), json=stats.to_json(orient='index'))
+ 
 
 schedule.every().minute.do(update_players_data)
+
 
 while True:
     schedule.run_pending()
