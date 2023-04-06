aaf4598bac322528f279ab5301c41e1734010cf2
Kirill Klem <klemkirill20@gmail.com>
2023-04-06 22:09:24 +0300

    changed saving

--- app.py
+++ app.py
@@ -14,14 +14,48 @@ fbref = sd.FBref(leagues=['ENG-Premier League'], seasons='22-23')
 
 url = URL(settings.BASE_API_URL) / 'api' / 'player'
 
-def update_players_data():
-    logger.debug('chto ugodno')
-    stats = fbref.read_player_season_stats(stat_type='standard')
-    r = requests.post(str(url), json=stats.to_json(orient='index'))
-
 def save_data():
-    all_players = []
+    all_players = {}
 
+    first_save(all_players)
+
+    for player in all_players:
+
+
+
+        save_stat_type('possession', all_players)
+        save_stat_type('playing_time', all_players)
+        save_stat_type('passing', all_players)
+        save_stat_type('passing_types', all_players)
+        
+        if all_players[player]['pos'] == 'GK':
+            save_stat_type('keeper', all_players)
+            save_stat_type('keeper_adv', all_players)
+
+        elif all_players[player]['pos'] == 'DF':
+            save_stat_type('misc', all_players)
+            save_stat_type('defense', all_players)
+
+        elif all_players[player]['pos'] == 'MF':
+            save_stat_type('misc', all_players)
+            save_stat_type('shooting', all_players)
+            save_stat_type('defense', all_players)
+            save_stat_type('goal_shot_creation', all_players)
+
+        elif all_players[player]['pos'] == 'FW':
+            save_stat_type('shooting', all_players)
+            save_stat_type('goal_shot_creation', all_players)
+
+        
+        # if player['pos'] == 'GK':
+        #     requests.post(str(url / 'GK'), json=player)
+
+    pprint(all_players)
+    # for player in all_players:
+    #     requests.post(str(url / player['pos']), json=player)
+        
+
+def first_save(all_players):
     stats = fbref.read_player_season_stats(stat_type='standard')
     players = stats.to_dict(orient='index')
 
@@ -30,46 +64,11 @@ def save_data():
 
         player['name'] = key[3]
         player['team'] = key[2]
-
-        if int(value[('Playing Time', 'MP')]) > 3:
+        if float(value[('Playing Time', '90s')]) > 0.5:
             for j, k in value.items():
                 player[('_'.join(' '.join(j).split()).lower().replace('%', '_percent').replace('+/-', '_plus_minus_').replace('/', '_in_').replace('+', '_plus_').replace('-', '_minus_'). replace('(', '').replace(')', '').replace(':', '_').replace('#', '_'))] = k
             edit_player(player)
-            all_players.append(player)
-
-
-    for player in all_players:
-
-        save_stat_type('possession', all_players)
-        save_stat_type('playing_time', all_players)
-        save_stat_type('passing', all_players)
-        save_stat_type('passing_types', all_players)
-        
-        if player['pos'] == 'GK':
-            save_stat_type('keeper', all_players)
-            save_stat_type('keeper_adv', all_players)
-
-        elif player['pos'] == 'DF':
-            save_stat_type('misc', all_players)
-            save_stat_type('defense', all_players)
-
-        elif player['pos'] == 'MF':
-            save_stat_type('misc', all_players)
-            save_stat_type('shooting', all_players)
-            save_stat_type('defense', all_players)
-            save_stat_type('goal_shot_creation', all_players)
-
-        elif player['pos'] == 'FW':
-            save_stat_type('shooting', all_players)
-            save_stat_type('goal_shot_creation', all_players)
-        
-        pprint(all_players)
-        if player['pos'] == 'GK':
-            requests.post(str(url / 'GK'), json=player)
-        pprint(all_players)
-        
-
-
+            all_players[player['name']] = player
 
 
 def save_stat_type(stat_type, all_players):
@@ -88,15 +87,18 @@ def save_stat_type(stat_type, all_players):
             if isinstance(k, float) and math.isnan(k):
                 player[j] = 0.0 
             
-            
         del player['pos']
         del player['age']
         del player['born']
         del player['nation']
 
-        for human in all_players:
-            if human['name'] == name:
-                human.update(player)
+        criterion = 'on90s'
+        if stat_type == 'playing_time' or stat_type == 'keeper':
+            criterion = 'playing_time_90s'
+
+        if float(player[criterion]) > 0.5:
+            all_players[name].update(player)
+        
 
 
 def edit_player(player):
